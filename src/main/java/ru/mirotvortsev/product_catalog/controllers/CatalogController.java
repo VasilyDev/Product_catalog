66873a5f89fb94a70f1a39d8d75b6fa9281ca4c2
vasilymirotvortsev <mirotvorcev.v@yandex.ru>
2021-01-08 17:36:24 +0300

    Added new features: editing, deleting products from the table catalog

--- src/main/java/ru/mirotvortsev/product_catalog/controllers/CatalogController.java
+++ src/main/java/ru/mirotvortsev/product_catalog/controllers/CatalogController.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.mirotvortsev.product_catalog.models.Product;
 import ru.mirotvortsev.product_catalog.repo.ProductRepository;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class CatalogController {
@@ -34,5 +38,34 @@ public class CatalogController {
         return "redirect:/";
     }
 
+    @GetMapping("/{id}/edit")
+    public String productEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!productRepository.existsById(id)) {
+            return "redirect:/";
+        }
+        Optional<Product> product = productRepository.findById(id);
+        ArrayList<Product> res = new ArrayList<>();
+        product.ifPresent(res::add);
+        model.addAttribute("product", res);
+        return "edit";
+    }
+
+
+    @PostMapping("/{id}/edit")
+    public String productPostUpdate(@PathVariable(value = "id") long id, @RequestParam String name, @RequestParam String description, Model model) {
+        Product product = productRepository.findById(id).orElseThrow(IllegalStateException::new);
+        product.setName(name);
+        product.setDescription(description);
+        productRepository.save(product);
+        return "redirect:/";
+    }
+
+
+    @PostMapping("/{id}/remove")
+    public String productPostDelete(@PathVariable(value = "id") long id, Model model) {
+        Product product = productRepository.findById(id).orElseThrow(IllegalStateException::new);
+        productRepository.delete(product);
+        return "redirect:/";
+    }
 
 }
